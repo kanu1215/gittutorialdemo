e2827e8a0f695da256e407c73df3dfd3c41429ba
kanu1215 <khuspeanuja@gmail.com>
2025-05-12 23:16:36 +0530

    added elementid files

---
+++ elementid/script.js
@@ -0,0 +1,19 @@
+// Write your code below:
+
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.color = 'orange';
+header.style.borderBottom = '3px solid orange';
+
+
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World';
+mainHeading.style.color = 'orange';
+
+
+const basket = document.getElementById('basket-heading');
+basket.style.color = 'green';
+
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</p>';
